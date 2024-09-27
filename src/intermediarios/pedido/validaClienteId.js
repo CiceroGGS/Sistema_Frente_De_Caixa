e4a812c1a03b097554fbc1cbf31fdf8ca3f801d2
cicero <cicerog.silvestre@gmail.com>
2024-09-26 21:26:07 -0300

    intermediario pedido e rota pedidos atualizados

---
+++ src/intermediarios/pedido/validaClienteId.js
@@ -0,0 +1,21 @@
+const knex = require("../../conexao");
+
+const validaIdPedido = async (req, res, next) => {
+    const { cliente_id } = req.body;
+
+    try {
+
+        const buscaCliente = await knex("clientes").where({ id: cliente_id }).first();
+
+        if (!buscaCliente) {
+            return res.status(400).json({ mensagem: "O ID do Cliente informado não existe." })
+        }
+
+        next();
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: `Ocorreu um erro interno do servidor: ${error.message}` });
+    }
+}
+
+module.exports = validaIdPedido;
